fc3a81a4ba78cfe31f607318ff4a1d3ec63decfd
Jack Watters <squawfreeskier@gmail.com>
2019-04-12 19:07:07 -0700

    React App Setup

---
+++ wds/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import {Route, BrowserRouter, Switch } from "react-router-dom";
+import './App.css';
+import landingPage from './Components/landingPage/landingPage';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={landingPage}/>
+        </Switch>
+      </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ wds/src/Components/landingPage/landingPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import '/Users/wubs/WDS/wds/src/App.css';
+
+class landingPage extends Component {
+    render() {
+        return (
+         <div>lol</div>  
+        )
+    }
+}
+export default landingPage;
