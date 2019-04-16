c60a8d80b7b4cf0c669997d4ca8d7db4037e3e2a
Jack Watters <squawfreeskier@gmail.com>
2019-04-16 01:18:47 -0700

    finishing landing page

--- wds/src/Components/landingPage/landingPage.js
+++ wds/src/Components/landingPage/landingPage.js
@@ -1,3 +1,5 @@
+import Particles from 'react-particles-js';
+
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import '/Users/wubs/WDS/wds/src/App.css';
@@ -17,15 +19,10 @@ import {
     DropdownItem ,
     Jumbotron,
     Container,
-    Card, 
-    CardImg, 
-    CardText, 
-    CardBody,
-    CardTitle, 
-    CardSubtitle, 
-    Button
 } from 'reactstrap';
-import firebase from "firebase";
+import SignUpPage from '../SignUp/SignUpForm';
+import Modalexport from '../Model/Model';
+import Particalexport from '../Particles/Partical';
 
 class landingPage extends Component {
     constructor(props) {
@@ -46,18 +43,18 @@ class landingPage extends Component {
         <div>
             <div>
              <Navbar color="lightblue" light expand="md">
-          <NavbarBrand href="/">WDS</NavbarBrand>
+          <NavbarBrand href="/">S.S</NavbarBrand>
           <NavbarToggler onClick={this.toggle} />
           <Collapse isOpen={this.state.isOpen} navbar>
             <Nav className="ml-auto" navbar>
             <NavItem>
-                <NavLink href="">Log in</NavLink>
+                <Modalexport></Modalexport>
               </NavItem>
               <NavItem>
                 <NavLink href="/">Home</NavLink>
               </NavItem>
               <NavItem>
-                <NavLink href="">About</NavLink>
+                <NavLink href="/About">About</NavLink>
               </NavItem>
               <UncontrolledDropdown nav inNavbar>
                 <DropdownToggle nav caret>
@@ -79,28 +76,37 @@ class landingPage extends Component {
           <div className="main">
             <Jumbotron className="bg-img">
                 <Container fluid>
-                <h1 className="display-2 ">World Development Solutions</h1>
+                <h1 className="display-2 ">Scalable Solutions</h1>
                 <p className="lead displa-2">Your Web-Development and Digital Marketing done correctly</p>
                 </Container>
             </Jumbotron>
           </div>
           <div>
+            <div id="container">
+          <video id="background-video" loop autoPlay>
+                <source src="6387.mp4" type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
+            </div> 
             <Paper className="paper">
-              <h6>About</h6>
-              <hr></hr>
-              <h6>Learn more about WDS, Web-Development and Digital Marketing</h6>
-            </Paper>
-            <Paper className="paper2">
               <h6>Sign up now!</h6>
               <hr></hr>
-              <h6>sign up:  </h6>
+              <SignUpPage></SignUpPage>
             </Paper>
-            <Paper className="paper3">
-              <h6>Contact Us:</h6>
+            <Paper className="paper2">
+              <h6>Contact our team</h6>
               <hr></hr>
+              
             </Paper>
           </div>
-
+          <footer id="footer">
+          <div id="foot"></div>
+            <i id="copyright">© 2019 Scalable Solutions llc</i>
+            <br></br>
+            <a href="https://www.instagram.com/?hl=en" id="insta" className="fab fa-instagram fa-2x"></a>
+            <a href="https://www.linkedin.com" className="fab fa-linkedin fa-2x" id="linked"></a>
+            <a href="https://www.facebook.com" className="fab fa-facebook-square fa-2x" id="face"></a>
+          </footer>
         </div>
         )
     }
