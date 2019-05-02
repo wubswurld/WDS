aab6a44b1b11e132b46225d238865d39c899f908
Jack Watters <squawfreeskier@gmail.com>
2019-05-02 03:05:02 -0700

    admin

--- wds/src/Components/CheckoutModel/Select.js
+++ wds/src/Components/CheckoutModel/Select.js
@@ -22,12 +22,12 @@ class ControlledOpenSelect extends React.Component {
   constructor(props) {
   super(props);
     this.state = {
-    Price: '',
     open: false,
-  }};
-  handleChange = (event) => {
-    this.setState({ Price: event.target.value });
-  };
+  }
+};
+  // handleChange = (event) => {
+  //   this.setState({ Price: event.target.value });
+  // };
 
   handleClose = () => {
     this.setState({ open: false });
@@ -38,8 +38,9 @@ class ControlledOpenSelect extends React.Component {
   };
 
   render() {
-    const { classes } = this.props;
-    console.log(this.state.Price);
+    //destructuring object
+    const { classes, controlOpen, controlClose, controlSelect, value, controlChange } = this.props;
+    // console.log(this.state.Price);
     return (
       <form autoComplete="off">
         <FormControl className={classes.formControl}>
@@ -48,25 +49,24 @@ class ControlledOpenSelect extends React.Component {
             open={this.state.open}
             onClose={this.handleClose}
             onOpen={this.handleOpen}
-            value={this.state.Price}
-            onChange={this.handleChange}
+            value={value}
+            onChange={controlChange}
             inputProps={{
-              name: 'Price',
+              name: 'data',
               id: 'demo-controlled-open-select',
             }}
           >
             <MenuItem value="">
               <em>None</em>
             </MenuItem>
-            <MenuItem value={600}>Basic | 600$ monthly</MenuItem>
-            <MenuItem value={800}>Super | 800$ monthly</MenuItem>
-            <MenuItem value={1200}>Premium | 1200$ monthly</MenuItem>
+            <MenuItem value={600}>Basic | 600$</MenuItem>
+            <MenuItem value={800}>Super | 800$</MenuItem>
+            <MenuItem value={1200}>Premium | 1200$</MenuItem>
           </Select>
         </FormControl>
       </form>
-    );
+    )};
   }
-}
 
 ControlledOpenSelect.propTypes = {
   classes: PropTypes.object.isRequired,
@@ -74,3 +74,80 @@ ControlledOpenSelect.propTypes = {
 
 export default withStyles(styles)(ControlledOpenSelect);
 
+// import React from 'react';
+// import PropTypes from 'prop-types';
+// import { withStyles } from '@material-ui/core/styles';
+// import InputLabel from '@material-ui/core/InputLabel';
+// import MenuItem from '@material-ui/core/MenuItem';
+// import FormControl from '@material-ui/core/FormControl';
+// import Select from '@material-ui/core/Select';
+// import Button from '@material-ui/core/Button';
+
+// const styles = theme => ({
+//   button: {
+//     display: 'block',
+//     marginTop: theme.spacing.unit * 2,
+//   },
+//   formControl: {
+//     margin: theme.spacing.unit,
+//     minWidth: 120,
+//   },
+// });
+
+// class ControlledOpenSelect extends React.Component {
+//   state = {
+//     age: '',
+//     open: false,
+//   };
+
+//   handleChange = event => {
+//     this.setState({ [event.target.name]: event.target.value });
+//   };
+
+//   handleClose = () => {
+//     this.setState({ open: false });
+//   };
+
+//   handleOpen = () => {
+//     this.setState({ open: true });
+//   };
+
+//   render() {
+//     const { classes } = this.props;
+
+//     return (
+//       <form autoComplete="off">
+//         <Button className={classes.button} onClick={this.handleOpen}>
+//           Open the select
+//         </Button>
+//         <FormControl className={classes.formControl}>
+//           <InputLabel htmlFor="demo-controlled-open-select">Age</InputLabel>
+//           <Select
+//             open={this.state.open}
+//             onClose={this.handleClose}
+//             onOpen={this.handleOpen}
+//             value={this.state.age}
+//             onChange={this.handleChange}
+//             inputProps={{
+//               name: 'age',
+//               id: 'demo-controlled-open-select',
+//             }}
+//           >
+//             <MenuItem value="">
+//               <em>None</em>
+//             </MenuItem>
+//             <MenuItem value={10}>Ten</MenuItem>
+//             <MenuItem value={20}>Twenty</MenuItem>
+//             <MenuItem value={30}>Thirty</MenuItem>
+//           </Select>
+//         </FormControl>
+//       </form>
+//     );
+//   }
+// }
+
+// ControlledOpenSelect.propTypes = {
+//   classes: PropTypes.object.isRequired,
+// };
+
+// export default withStyles(styles)(ControlledOpenSelect);
