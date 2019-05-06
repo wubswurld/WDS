080c802c4d7985301a0b11c1fb37572c3cd194d3
Jack Watters <squawfreeskier@gmail.com>
2019-05-06 03:32:41 -0700

    lol

---
+++ wds/src/Components/Packs/UserInfo.js
@@ -0,0 +1,26 @@
+// import React, { Component } from 'react';
+
+// const UserInfo = ({ authUser }) => (
+    
+//     <div>
+//         <Userstuff authUser={authUser} />
+//     </div>
+// );
+
+// class Userstuff extends Component {
+//     render () {
+//         const { authUser } = this.props;
+//         console.log(authUser)
+//         return (
+//             <div>
+//                 <p>
+//                     username: 
+//                 </p>
+//             </div>
+//         )
+//     }
+// }
+
+// export { Userstuff };
+
+// export default UserInfo;
