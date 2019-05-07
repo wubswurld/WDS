16870cb8ed68dcf00be9b36e023d7a9131d81416
Jack Watters <squawfreeskier@gmail.com>
2019-05-06 17:32:48 -0700

    dialog details

--- wds/src/Components/Navbar/DialogDetails.js
+++ wds/src/Components/Navbar/DialogDetails.js
@@ -3,17 +3,22 @@ import React, { Component } from 'react';
 import SignOutButton from '../signOut/index'
 import { ChangePasswordLink } from './NavDialog'
 import { withFirebase } from '../Firebase';
+import { userInfo } from 'os';
+import { object } from 'prop-types';
 
 
 
 
 const DialogDetails = ({ authUser, UserInfo }) => (
     <>
+    <p>Unique Id: {authUser.uid}</p>
+    <hr></hr>
     <p>email: {authUser.email}</p>
     <hr></hr>
     <p>Name: {UserInfo.username}</p>
     <hr></hr>
-    <p>Last Payment: </p>
+    <p>Last Paymennt: {UserInfo.paymentDate}</p>
+    <p>payment details: {UserInfo.payment.paymentID}</p>
     <div>
     <ChangePasswordLink></ChangePasswordLink>
     <SignOutButton></SignOutButton>
@@ -27,7 +32,7 @@ class Contain extends Component {
     constructor(props) {
         super(props);
         this.state = {...Add_To};
-        console.log(this.state);
+        // console.log(this.state);
     }
     componentDidMount() {
         this.props.firebase.users().on('value', snapshot => {
@@ -41,17 +46,22 @@ class Contain extends Component {
             // if (userObject[k].email === valuetomatch) {
             //     return true;
             // }
-            const match = userList.find((userList) => userList.email == this.props.authUser.email);
+            const match = userList.find((userList) => userList.email == this.props.authUser.email); 
+            const newmatch = match.payment;
+            console.log(newmatch);
+            const newList = Object.values(newmatch);
+            console.log(newList);
             this.setState({
                 UserInfo: match
             })
-            console.log(match);
+            // console.log(match);
         });
 }
     render() {
         const { authUser } = this.props;
         const { UserInfo } = this.state;
-        console.log(UserInfo);
+        console.log(UserInfo)
+        // console.log(UserInfo);
         return (
             <>
             <DialogDetails authUser={authUser} UserInfo={UserInfo}></DialogDetails>
